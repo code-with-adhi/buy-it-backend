b6cc9a56fb18b16158ab36ae1c86c1bc1bdfcf58
code-with-adhi <125806079+code-with-adhi@users.noreply.github.com>
2025-09-07 17:25:51 +0530

    cors configuration

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const cartRoutes = require("./routes/cart.routes.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+};
+app.use(cors(corsOptions));
 
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
